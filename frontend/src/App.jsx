0968a01aed0930e886f2301541f39946b03f40d5
Shivendra <67376433+shivendrakc@users.noreply.github.com>
2025-04-03 11:17:26 +1100

    Changd the path name as there was a error in case

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 import CreatePage from "./Pages/CreatePage";
 import Homepage from "./Pages/Homepage";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 function App() {
   return (
       <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
